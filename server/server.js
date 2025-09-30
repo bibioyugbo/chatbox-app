5c924f2ac8f20396fc8820208567fe43945cb4d3
bibi oyugbo <obehioyeoyugbo@gmail.com>
2025-09-30 22:39:56 +0100

    Fixed * route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cors({
 
 app.use(express.static(path.join(__dirname, "client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
 app.use(express.json());
